cb20474c71796da19839a45e051022fe7ddeb62e
Nikita Somenkov <somenkov.nikita@icloud.com>
2019-07-03 21:45:38 +0300

    Use render instead templates directly

--- clock-web/handlers/clock.py
+++ clock-web/handlers/clock.py
@@ -1,12 +1,7 @@
-import tornado.template
 import tornado.web
 
 
 class ClockHandler(tornado.web.RequestHandler):
-    def initialize(self):
-        self.loader = tornado.template.Loader("static/templates")
-
     def get(self):
-        template = self.loader.load("index.html")
-        self.write(template.generate(title="clock-web", time="15:25"))
+        self.render("index.html", title="clock-web")
 
