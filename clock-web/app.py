380e5b00b08e8f313c68e9dea14371438d5dec75
Nikita Somenkov <somenkov.nikita@icloud.com>
2019-07-03 19:19:27 +0300

    Add clock handlers

--- clock-web/app.py
+++ clock-web/app.py
@@ -6,7 +6,7 @@ import tornado.ioloop
 import tornado.options
 
 import handlers.api
-
+import handlers.clock
 
 tornado.options.define("port", default=8080, help="port to listen on")
 
@@ -14,6 +14,7 @@ tornado.options.define("port", default=8080, help="port to listen on")
 def make_clock_web_app():
     return tornado.web.Application([
         (r"/api", handlers.api.ApiHandler),
+        (r"/clock", handlers.clock.ClockHandler),
     ])
 
 
