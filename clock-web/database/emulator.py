72ed324ab5b7c6458d3ea49abfe7de9cfe0125df
Nikita Somenkov <somenkov.nikita@icloud.com>
2019-07-07 01:16:39 +0300

    Add stop method in emulator

--- clock-web/database/emulator.py
+++ clock-web/database/emulator.py
@@ -10,12 +10,17 @@ class TimeDBActivityEmulator:
     def __init__(self, timedb: timedb.TimeDB):
         self.timedb = timedb
         self.thread = threading.Thread(target=self._activity_emulate)
+        self.stop_need = False
 
     def start(self):
         self.thread.start()
 
+    def stop(self):
+        self.stop_need = True
+
     def _activity_emulate(self):
-        while True:
+        while self.stop_need:
             self.timedb.push_time(time.strftime('%X %x %Z'))
-            time.sleep(1)
+            if not self.stop_need:
+                time.sleep(1)
 
